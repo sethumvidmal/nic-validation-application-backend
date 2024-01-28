32a0a82d7f909b9eba9443216dfdfc9a61529d24
Sethum Vidmal <126678663+sethumvidmal@users.noreply.github.com>
2024-01-28 12:46:41 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,8 +10,4 @@ public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class);
     }
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
 }
